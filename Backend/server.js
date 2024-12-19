b9acdbd8e699ea0e45af18bc9db9040a586276bf
Anand Pathak <pathakanand2222@gmail.com>
2024-12-19 14:31:47 +0530

    basic backend

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const http=require('http');
+const app=require('./app');
+
+const server=http.createServer(app);
+
+const port =process.env.PORT||8000
+
+server.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+})
